251be41d5b417494fc0903b9fa5d20cb0fe2c013
Manahil-Abid <manahilabid990@gmail.com>
2025-01-01 22:54:51 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 preview = () => {
-
     let firstName = document.getElementById("firstName").value;
-    let lastName = document.getElementById("lastName").value;
-    let email = document.getElementById("email").value;
-    let phone = document.getElementById("phone").value;
-    let message = document.getElementById("message").value;
-
+let lastName = document.getElementById("lastName").value;
+let email = document.getElementById("email").value;
+let phone = document.getElementById("phone").value;
+let message = document.getElementById("message").value;
 
     document.getElementById("previewFirstName").textContent = firstName;
     document.getElementById("previewLastName").textContent = lastName;
@@ -15,4 +13,16 @@ preview = () => {
 
 
     document.getElementById("previews").style.display = "block";
+}
+submit = () => {
+
+    document.getElementById("firstName").value = "";
+   document.getElementById("lastName").value  = "";
+   document.getElementById("email").value  = "";
+   document.getElementById("phone").value  = "";
+   document.getElementById("message").value  = "";
+
+   document.getElementById("previews").style.display = "none";
+
+   
 }
